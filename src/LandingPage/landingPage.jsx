1d83c87a436b789b0f44391b0657d8e813b1c086
naraveni <harshaemployment@gmail.com>
2024-12-01 19:39:56 -0500

    Second COmmit

---
+++ src/LandingPage/landingPage.jsx
@@ -0,0 +1,12 @@
+import indianStudentsImage from '../assets/indianStudents.avif'
+
+function LandingPage(){
+
+  return(
+   <div>
+     <img src={indianStudentsImage}></img>
+   </div>
+  )
+
+}
+export default LandingPage
